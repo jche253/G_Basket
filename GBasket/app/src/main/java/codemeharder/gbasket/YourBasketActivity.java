e6e4c37058487ed8d1c0a5d49993b37ba467a8a5
jche253 <jimmy.jim.hi@gmail.com>
2016-02-14 23:28:21 -0500

    Revert "Revert "Jimmy Update 2""
    This reverts commit c6b27448f93ece50994fb9ed3dbf95cfba8da910.

--- GBasket/app/src/main/java/codemeharder/gbasket/YourBasketActivity.java
+++ GBasket/app/src/main/java/codemeharder/gbasket/YourBasketActivity.java
@@ -3,16 +3,19 @@ package codemeharder.gbasket;
 import android.app.Activity;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.ListView;
 
 /**
  * Created by Jimmy Chen on 2/14/2016.
  */
 public class YourBasketActivity extends Activity {
+    ListView lv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_yourbucket);
 
+        lv = (ListView) findViewById(R.id.listView);
 
 
     }
