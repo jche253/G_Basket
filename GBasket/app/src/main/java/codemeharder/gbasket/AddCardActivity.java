e48f09b07cdd5629e82a301d498d91bb686f46c7
Seo Young Kyung <seo.kyung613@gmail.com>
2016-02-19 13:23:49 -0500

    payment & add card pg updated
    connected buttons

--- GBasket/app/src/main/java/codemeharder/gbasket/AddCardActivity.java
+++ GBasket/app/src/main/java/codemeharder/gbasket/AddCardActivity.java
@@ -2,11 +2,13 @@ package codemeharder.gbasket;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
 
 import com.paypal.android.sdk.payments.PayPalConfiguration;
 
 /**
  * Created by Jimmy Chen on 2/18/2016.
+ * Edited by Seoyoung Kyung on 2/19/2016.
  */
 public class AddCardActivity extends Activity {
     // Can be NO_NETWORK for OFFLINE, SANDBOX for TESTING and LIVE for PRODUCTION
@@ -16,12 +18,16 @@ public class AddCardActivity extends Activity {
     // when testing in sandbox, this is likely the -facilitator email address.
     private static final String CONFIG_RECEIVER_EMAIL = "jche253-facilitator@emory.edu";
 
-    //TODO add the card buttons and link (Sprint 2/3)
+    Button submit, cancel;
+
+
     //TODO Process credit card + add it to saved list of cards for the user
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_addcard);
 
+        submit = (Button) findViewById(R.id.btnSubmit);
+        cancel = (Button) findViewById(R.id.btnCancel);
     }
 }
--- GBasket/app/src/main/java/codemeharder/gbasket/PaymentActivity.java
+++ GBasket/app/src/main/java/codemeharder/gbasket/PaymentActivity.java
@@ -2,11 +2,13 @@ package codemeharder.gbasket;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
 
 import com.paypal.android.sdk.payments.PayPalConfiguration;
 
 /**
  * Created by Jimmy Chen on 2/18/2016.
+ * Edited by Seoyoung Kyung on 2/19/2016.
  */
 public class PaymentActivity extends Activity {
     // Can be NO_NETWORK for OFFLINE, SANDBOX for TESTING and LIVE for PRODUCTION
@@ -16,11 +18,16 @@ public class PaymentActivity extends Activity {
     // when testing in sandbox, this is likely the -facilitator email address.
     private static final String CONFIG_RECEIVER_EMAIL = "jche253-facilitator@emory.edu";
 
-    //TODO Make rest of receipt layouts and connect buttons
+    Button Paywcard, Addcard;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_addcard);
+
+        Paywcard = (Button) findViewById(R.id.ButtonPaywcard);
+        Addcard = (Button) findViewById(R.id.ButtonAddcard);
+
 
     }
 }
