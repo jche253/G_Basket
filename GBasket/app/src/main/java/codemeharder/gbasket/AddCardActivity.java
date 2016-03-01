7b3a4a68cefb6f60077077c2974197c91a169a1e
jche253 <jimmy.jim.hi@gmail.com>
2016-02-29 23:49:08 -0500

    Adding Stripe
    Set up AddCardActivity to validate card (don't know if it works for
    actual cards, just know that it fails invalid ones) and also some layout
    fixes

--- GBasket/app/src/main/java/codemeharder/gbasket/AddCardActivity.java
+++ GBasket/app/src/main/java/codemeharder/gbasket/AddCardActivity.java
@@ -1,12 +1,18 @@
 package codemeharder.gbasket;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-import com.paypal.android.sdk.payments.PayPalConfiguration;
+import com.stripe.android.*;
+import com.stripe.android.model.Card;
 
 /**
  * Created by Jimmy Chen on 2/18/2016.
@@ -16,7 +22,7 @@ public class AddCardActivity extends Activity {
 
 
     Button addCard, cancel;
-
+    EditText name, card, eMonth, eYear, CV;
 
     //TODO Process credit card + add it to saved list of cards for the user
     @Override
@@ -26,13 +32,67 @@ public class AddCardActivity extends Activity {
 
         addCard = (Button) findViewById(R.id.btnSubmit);
         cancel = (Button) findViewById(R.id.btnCancel);
+        name = (EditText) findViewById(R.id.name);
+        card = (EditText) findViewById(R.id.ccNum);
+        eMonth = (EditText) findViewById(R.id.expMonth);
+        eYear = (EditText) findViewById(R.id.expYear);
+        CV = (EditText) findViewById(R.id.CVC);
+
 
         addCard.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), PaymentActivity.class);
-                //Todo add credit card to listView
-                startActivity(intent);
+                String UserName = name.getText().toString();
+                String CreditCardNum = card.getText().toString();
+                int ExpireMonth = 0;
+                try {
+                    ExpireMonth = Integer.parseInt(eMonth.getText().toString());
+                } catch (NumberFormatException e){
+                    ExpireMonth = 0;
+                }
+
+                int ExpireYear = 0;
+                try {
+                    ExpireYear = Integer.parseInt(eYear.getText().toString());
+                } catch (NumberFormatException e){
+                    ExpireYear = 0;
+                }
+
+                String CVV = CV.getText().toString();
+
+                Card CreditCard = new Card(CreditCardNum, ExpireMonth, ExpireYear, CVV);
+
+               /* if (UserName == null || CreditCardNum == null || !(CreditCardNum.matches("^-?\\d+$"))
+                        || CVV == null || !(CVV.matches("^-?\\d+$"))
+                        || ExpireMonth <= 0 || ExpireYear <= 0)*/
+
+                //Check if card is valid
+                if(!CreditCard.validateCard() || !CreditCard.validateCVC() ||
+                        !CreditCard.validateExpiryDate()){
+                    new AlertDialog.Builder(AddCardActivity.this)
+                            .setTitle("Invalid Card")
+                            .setMessage("Please enter a valid card")
+                            .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int which) {
+                                    //Reset form
+                                    name.setText("");
+                                    card.setText("");
+                                    eMonth.setText("");
+                                    eYear.setText("");
+                                    CV.setText("");
+                                }
+                            })
+                            .setIcon(android.R.drawable.ic_dialog_alert)
+                            .show();
+
+                }
+                else {
+                        //Card is good, can move on to Payment form
+                        Intent intent = new Intent(getApplicationContext(), PaymentActivity.class);
+
+                        //Todo add credit card to listView
+                        startActivity(intent);
+                }
             }
         });
 
--- GBasket/app/src/main/java/codemeharder/gbasket/PaymentActivity.java
+++ GBasket/app/src/main/java/codemeharder/gbasket/PaymentActivity.java
@@ -1,18 +1,25 @@
 package codemeharder.gbasket;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
+import com.stripe.android.*;
+import com.stripe.android.model.Card;
+import com.stripe.android.model.Token;
+import com.stripe.exception.AuthenticationException;
 
+import android.widget.Toast;
+/*
 import com.paypal.android.sdk.payments.PayPalConfiguration;
 import com.paypal.android.sdk.payments.PayPalPayment;
 import com.paypal.android.sdk.payments.PayPalService;
 import com.paypal.android.sdk.payments.PaymentConfirmation;
-
+*/
 import org.json.JSONException;
 
 import java.math.BigDecimal;
@@ -24,17 +31,6 @@ import java.math.BigDecimal;
  */
 
 public class PaymentActivity extends Activity {
-    // Can be NO_NETWORK for OFFLINE, SANDBOX for TESTING and LIVE for PRODUCTION
-    private static final String CONFIG_ENVIRONMENT = PayPalConfiguration.ENVIRONMENT_SANDBOX;
-    // note that these credentials will differ between live & sandbox environments.
-    private static final String CONFIG_CLIENT_ID = "AWtrDxQQ7r2j2oH-XRe-KVEgluiWXLbPwvZFOQpky9vHD23FcwqRJGnXzR4MC1WdlMJW0QlgWa4hkZ_R";
-    // when testing in sandbox, this is likely the -facilitator email address.
-    private static final String CONFIG_RECEIVER_EMAIL = "jche253-facilitator@emory.edu";
-    private static final int REQUEST_CODE_PAYMENT = 1;
-
-    private static PayPalConfiguration config = new PayPalConfiguration()
-            .environment(CONFIG_ENVIRONMENT)
-            .clientId(CONFIG_CLIENT_ID);
 
     Button Paywcard, Addcard;
 
@@ -46,17 +42,57 @@ public class PaymentActivity extends Activity {
         Paywcard = (Button) findViewById(R.id.ButtonPaywcard);
         Addcard = (Button) findViewById(R.id.ButtonAddcard);
 
-        Intent intent = new Intent(this, PayPalService.class);
-        intent.putExtra(PayPalService.EXTRA_PAYPAL_CONFIGURATION, config);
-        startService(intent);
-
         Paywcard.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent receiptIntent = new Intent(getApplicationContext(), ReceiptActivity.class);
+                String CreditCardNum = null;
+                int expMonth = 0;
+                int expYear = 0;
+                String CVC = null;
+                //TODO select card as item and initialize variables
+
                 //TODO add payment
-                onBuyPressed();
-                startActivity(receiptIntent);
+                //Parameters: string credit card number, int exp month, int exp year, string cvc
+                Card card = new Card(CreditCardNum, expMonth, expYear, CVC);
+                if (!card.validateCard()) {
+                    //Errors
+                    new AlertDialog.Builder(PaymentActivity.this)
+                            .setTitle("Invalid Card")
+                            .setMessage("Please enter a valid card")
+                            .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int which) {
+                                    //TODO delete card from list
+                                }
+                            })
+                            .setIcon(android.R.drawable.ic_dialog_alert)
+                            .show();
+
+                }
+                else {
+                    Stripe stripe = null;
+                    try {
+                        stripe = new Stripe("pk_test_2dYE7FzwvBwbxWNCdWtetXTp");
+                    } catch (AuthenticationException e) {
+                        e.printStackTrace();
+                    }
+                    assert stripe != null;
+                    stripe.createToken(
+                            card,
+                            new TokenCallback() {
+                                public void onSuccess(Token token) {
+                                    // Send token to your server
+
+                                }
+
+                                public void onError(Exception error) {
+                                    // Show localized error message
+
+                                }
+                            });
+                    Intent receiptIntent = new Intent(getApplicationContext(), ReceiptActivity.class);
+                    startActivity(receiptIntent);
+                }
+
             }
         });
 
@@ -69,53 +105,5 @@ public class PaymentActivity extends Activity {
             }
         });
     }
-    public void onBuyPressed() {
-        PayPalPayment thingToBuy = getThingToBuy(PayPalPayment.PAYMENT_INTENT_SALE);
-        Intent intent = new Intent(PaymentActivity.this, com.paypal.android.sdk.payments.PaymentActivity.class);
-        intent.putExtra(PayPalService.EXTRA_PAYPAL_CONFIGURATION, config);
-        intent.putExtra(com.paypal.android.sdk.payments.PaymentActivity.EXTRA_PAYMENT, thingToBuy);
-        startActivityForResult(intent, REQUEST_CODE_PAYMENT);
-    }
 
-    //Right now, this is just a temporary function
-    private PayPalPayment getThingToBuy(String paymentIntent) {
-        return new PayPalPayment(new BigDecimal("1.75"), "USD", "sample item",
-                paymentIntent);
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == REQUEST_CODE_PAYMENT) {
-            if (resultCode == Activity.RESULT_OK) {
-                PaymentConfirmation confirm =
-                        data.getParcelableExtra(com.paypal.android.sdk.payments.PaymentActivity.EXTRA_RESULT_CONFIRMATION);
-                if (confirm != null) {
-                    try {
-                        Log.e("Show", confirm.toJSONObject().toString(4));
-                        Log.e("Show", confirm.getPayment().toJSONObject().toString(4));
-                        /**
-                         *  TODO: send 'confirm' (and possibly confirm.getPayment() to your server for verification
-                         */
-                        Toast.makeText(getApplicationContext(), "PaymentConfirmation info received" +
-                                " from PayPal", Toast.LENGTH_LONG).show();
-                    } catch (JSONException e) {
-                        Toast.makeText(getApplicationContext(), "an extremely unlikely failure" +
-                                " occurred:", Toast.LENGTH_LONG).show();
-                    }
-                }
-            } else if (resultCode == Activity.RESULT_CANCELED) {
-                Toast.makeText(getApplicationContext(), "The user canceled.", Toast.LENGTH_LONG).show();
-            } else if (resultCode == com.paypal.android.sdk.payments.PaymentActivity.RESULT_EXTRAS_INVALID) {
-                Toast.makeText(getApplicationContext(), "An invalid Payment or PayPalConfiguration" +
-                        " was submitted. Please see the docs.", Toast.LENGTH_LONG).show();
-            }
-        }
-    }
-
-    @Override
-    public void onDestroy() {
-        // Stop service when done
-        stopService(new Intent(this, PayPalService.class));
-        super.onDestroy();
-    }
 }
