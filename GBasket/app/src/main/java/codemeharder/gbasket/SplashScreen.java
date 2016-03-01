d29f196d008c5dbccdc569aea8cfe6048ca7702e
Janay <jdevill@emory.edu>
2016-02-29 22:35:01 -0500

    Splash Screen
    Splash screen opens while app is loading.

---
+++ GBasket/app/src/main/java/codemeharder/gbasket/SplashScreen.java
@@ -0,0 +1,43 @@
+package codemeharder.gbasket;
+
+/**
+ * Created by jdevillasee on 2/26/16.
+ */
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.app.Activity;
+
+
+public class SplashScreen extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_screen);
+
+        Thread timerThread = new Thread(){
+            public void run(){
+                try{
+                    sleep(3000);
+                }catch(InterruptedException e){
+                    e.printStackTrace();
+                }finally{
+                    Intent intent = new Intent(SplashScreen.this,MainActivity.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        timerThread.start();
+    }
+
+    @Override
+    protected void onPause() {
+        // TODO Auto-generated method stub
+        super.onPause();
+        finish();
+    }
+
+}
+
