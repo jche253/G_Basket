3246c8203c23f0e7a917bac6fc128ac8438c3746
Seo Young Kyung <seo.kyung613@gmail.com>
2016-04-05 17:45:29 -0400

    minor stuff

--- GBasket/app/src/main/java/codemeharder/gbasket/AddCardActivity.java
+++ GBasket/app/src/main/java/codemeharder/gbasket/AddCardActivity.java
@@ -9,7 +9,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-import com.stripe.android.model.Card;
 
 /**
  * Created by Jimmy Chen on 2/18/2016.\
