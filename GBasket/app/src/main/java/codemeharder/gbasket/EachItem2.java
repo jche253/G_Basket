4ff67cf7f2389db82c1f7623d218c015794e2232
ahwong2 <Alfred.wong@emory.edu>
2016-04-05 23:39:43 -0400

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	GBasket/app/src/main/java/codemeharder/gbasket/AccountHelper.java
    #	GBasket/app/src/main/java/codemeharder/gbasket/ItemActivity.java
    #	GBasket/app/src/main/java/codemeharder/gbasket/LaunchPadActivity.java
    #	GBasket/app/src/main/java/codemeharder/gbasket/MainActivity.java
    #	GBasket/app/src/main/java/codemeharder/gbasket/SignUpActivity.java
    
    #	GBasket/endpoint/build/classes/endpointsSrc/com/example/jimmychen/myapplication/endpoint/myApi/MyApi$Builder.class
    
    #	GBasket/endpoint/build/classes/endpointsSrc/com/example/jimmychen/myapplication/endpoint/myApi/MyApi$SayHi.class
    
    #	GBasket/endpoint/build/classes/endpointsSrc/com/example/jimmychen/myapplication/endpoint/myApi/MyApi.class
    
    #	GBasket/endpoint/build/classes/endpointsSrc/com/example/jimmychen/myapplication/endpoint/myApi/model/MyBean.class
    #	GBasket/endpoint/build/client-libs/myApi-v1-java.zip
    
    #	GBasket/endpoint/build/generated-source/endpoints/java/com/example/jimmychen/myapplication/endpoint/myApi/MyApi.java
    
    #	GBasket/endpoint/build/generated-source/endpoints/java/com/example/jimmychen/myapplication/endpoint/myApi/MyApiRequest.java
    
    #	GBasket/endpoint/build/generated-source/endpoints/java/com/example/jimmychen/myapplication/endpoint/myApi/MyApiRequestInitializer.java
    
    #	GBasket/endpoint/build/generated-source/endpoints/java/com/example/jimmychen/myapplication/endpoint/myApi/MyApiScopes.java
    
    #	GBasket/endpoint/build/generated-source/endpoints/java/com/example/jimmychen/myapplication/endpoint/myApi/model/MyBean.java
    #	GBasket/endpoint/build/libs/endpoint-android-endpoints.jar
    
    #	GBasket/endpoint/build/tmp/appengineEndpointsExpandClientLibs/myApi-v1-java.zip-unzipped/myApi/src/main/java/com/example/jimmychen/myapplication/endpoint/myApi/MyApi.java
    
    #	GBasket/endpoint/build/tmp/appengineEndpointsExpandClientLibs/myApi-v1-java.zip-unzipped/myApi/src/main/java/com/example/jimmychen/myapplication/endpoint/myApi/MyApiRequest.java
    
    #	GBasket/endpoint/build/tmp/appengineEndpointsExpandClientLibs/myApi-v1-java.zip-unzipped/myApi/src/main/java/com/example/jimmychen/myapplication/endpoint/myApi/MyApiRequestInitializer.java
    
    #	GBasket/endpoint/build/tmp/appengineEndpointsExpandClientLibs/myApi-v1-java.zip-unzipped/myApi/src/main/java/com/example/jimmychen/myapplication/endpoint/myApi/MyApiScopes.java
    
    #	GBasket/endpoint/build/tmp/appengineEndpointsExpandClientLibs/myApi-v1-java.zip-unzipped/myApi/src/main/java/com/example/jimmychen/myapplication/endpoint/myApi/model/MyBean.java

---
+++ GBasket/app/src/main/java/codemeharder/gbasket/EachItem2.java
@@ -0,0 +1,42 @@
+package codemeharder.gbasket;
+
+import java.io.Serializable;
+
+/**
+ * Created by Alfred Wong on 4/5/2016.
+ */
+
+    public class EachItem2 implements Serializable {
+        //Each item on the ListView for YourBucket
+        String name;
+        double price;
+        boolean checkbox;
+        int ID;
+
+        public EachItem2(int ID, String name, double price, boolean checkBox) {
+            this.ID = ID;
+            this.name = name;
+            this.price = price;
+            this.checkbox = checkBox;
+        }
+        public EachItem2(){
+
+        }
+        public String getName(){
+            return this.name;
+        }
+        public double getPrice(){
+            return this.price;
+        }
+        public boolean getCheckBox() {return this.checkbox;}
+
+        public int getID(){return this.ID;}
+
+        public void setName(String name) {this.name = name;}
+        public void setID(int ID ) {this.ID = ID;}
+        public void setPrice(double price) {this.price = price;}
+
+
+    }
+
+
